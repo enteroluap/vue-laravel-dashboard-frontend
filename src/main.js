e7dee2ab138faedfdcdd41c552a2e638895524c5
enteroluap <pauloaugustin@gmail.com>
2023-12-22 01:22:21 -0300

    updating our frontend so far

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import LoginComponent from './views/LoginComponent.vue'
+import HomeComponent from './views/HomeComponent.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', name: 'login', component: LoginComponent },
+    { path: '/home', name: 'home', component: HomeComponent }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/LoginComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">
+
+s
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>>
+
+<style lang="scss" src="./styles.scss" scoped />
